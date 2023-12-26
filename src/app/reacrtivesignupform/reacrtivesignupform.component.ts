b30183aa773995b5cc6d5eb7b71d23ff2ef426eb
Abdul M Gill <amgill@cloudarchitect.net>
2023-12-25 20:22:15 -0600

    KV R : Reactive form changes

--- src/app/reacrtivesignupform/reacrtivesignupform.component.ts
+++ src/app/reacrtivesignupform/reacrtivesignupform.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-reacrtivesignupform',
@@ -7,16 +7,38 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrl: './reacrtivesignupform.component.css'
 })
 export class ReacrtivesignupformComponent {
-  signupForm: FormGroup  = new FormGroup({
-      'firstname' : new FormControl("", [Validators.required, Validators.minLength(5)]),
-      'lastname' : new FormControl("", [Validators.required, Validators.minLength(5)]),
-      'username' : new FormControl("", [Validators.required, Validators.minLength(5)]),
-      'password' : new FormControl("", [Validators.required, Validators.minLength(5)]),
-      'reEnterPassword' : new FormControl("", [Validators.required, Validators.minLength(5)]),
-      'email' : new FormControl("", [Validators.required]),
-      'mobile' : new FormControl("", [Validators.required]),
-      'dob' : new FormControl("", [Validators.required]),
-  });
+     //formBuilder : FormBuilder;
+
+     constructor(private formBuilder : FormBuilder){
+          //this.formBuilder = formBuilder;
+     }
+
+
+  // signupForm: FormGroup  = new FormGroup({
+  //     'firstname' : new FormControl("", [Validators.required, Validators.minLength(5)]),
+  //     'lastname' : new FormControl("", [Validators.required, Validators.minLength(5)]),
+  //     'username' : new FormControl("", [Validators.required, Validators.minLength(5)]),
+  //     'password' : new FormControl("", [Validators.required, Validators.minLength(5)]),
+  //     'reEnterPassword' : new FormControl("", [Validators.required, Validators.minLength(5)]),
+  //     'email' : new FormControl("", [Validators.required, Validators.email]),
+  //     'mobile' : new FormControl("", [Validators.required]),
+  //     'dob' : new FormControl("", [Validators.required]),
+  //     'gender' : new FormControl("", [Validators.required])
+  // });
+
+  signupForm: FormGroup  = this.formBuilder.group({
+    firstname : ["", [Validators.required, Validators.minLength(5)]],
+    lastname : ["",  [Validators.required, Validators.minLength(5)]],
+    username : ["", [Validators.required, Validators.minLength(5)]],
+    password : ["", [Validators.required, Validators.minLength(5)]],
+    reEnterPassword : ["", [Validators.required, Validators.minLength(5)]],
+    email : ["", [Validators.required, Validators.email]],
+    mobile : ["", [Validators.required]],
+    dob : ["", [Validators.required]],
+    gender : ["", [Validators.required]]
+});
+
+
 
   processSignupData() {
     console.log(this.signupForm.value);
